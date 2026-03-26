cda3449b591eecdd2bc16919fcb540b90334ec8c
maaz <maaz.cs05@gmail.com>
2026-03-27 00:21:51 +0530

    updated load testing for multiple user simulation

--- auth.py
+++ auth.py
@@ -5,5 +5,5 @@ from config import VALID_API_KEYS
 
 def validate_api_key(api_key: str):
     print("FUNCTION LOADED")
-    if api_key not in VALID_API_KEYS:
+    if not (api_key == "test123" or api_key.startswith("user")):
         raise HTTPException(status_code=401, detail="Invalid API Key")
--- load_test.js
+++ load_test.js
@@ -1,16 +1,30 @@
 import http from 'k6/http';
+import { sleep } from 'k6';
+
+const users = ["user1", "user2", "user3", "user4", "user5"];
 
 export default function () {
+    const user = users[Math.floor(Math.random() * users.length)];
+
+    const prompts = [
+        "Explain gravity",
+        "What is AI?",
+        "Tell me a joke",
+        "Explain black holes"
+    ];
+
+    const prompt = prompts[Math.floor(Math.random() * prompts.length)];
+
     http.post(
         'http://127.0.0.1:8000/generate',
-        JSON.stringify({
-            prompt: "Explain gravity"
-        }),
+        JSON.stringify({ prompt }),
         {
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer test123'
+                'Authorization': `Bearer ${user}`
             }
         }
     );
+
+    sleep(1);
 }
